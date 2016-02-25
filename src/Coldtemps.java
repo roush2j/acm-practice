6f6513e42d4724f2d1ba789c7d2c8166fdae5c78
JRoush <@>
2016-02-24 22:28:00 -0500

    Cold temperature checks

---
+++ src/Coldtemps.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+// Submissions to CMICH SP16 #2
+
+public class Coldtemps {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int count = s.nextInt();
+        int belowz = 0;
+        for (; count > 0; count--) {
+            int t = s.nextInt();
+            if (t < 0) belowz++;
+        }
+        System.out.println(belowz);
+        s.close();
+    }
+}
