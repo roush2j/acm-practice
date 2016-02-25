05879d3a86d5a4716466233dd96cab2742a77005
JRoush <@>
2016-02-24 22:32:54 -0500

    worlds dumbest 'secure' erasure scheme

---
+++ src/CrapErase.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+// Submissions to CMICH SP16 #2
+
+public class CrapErase {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        boolean nop = (n % 2) == 0;
+        s.nextLine();
+        String in = s.nextLine();
+        String out = s.nextLine();
+        for (int i = 0; i < in.length(); i++) {
+            char cin = in.charAt(i);
+            char cout = nop ? cin : cin == '0' ? '1' : '0';
+            if (i >= out.length() || out.charAt(i) != cout) {
+                System.out.println("Deletion failed");
+                s.close();
+                return;
+            }
+        }
+        System.out.println("Deletion succeeded");
+        s.close();
+    }
+}
