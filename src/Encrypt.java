00aa4cb7a9428cf59036513041a07bc4a892c9d2
JRoush <@>
2016-02-24 22:31:31 -0500

    character reordering encryption

---
+++ src/Encrypt.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+// Submissions to CMICH SP16 #2
+
+public class Encrypt {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int count = s.nextInt();
+        s.nextLine();
+        for (; count > 0; count--) {
+            String msg = s.nextLine();
+            int k = (int) Math.ceil(Math.sqrt(msg.length()));
+            for (int x = 0; x < k; x++) {
+                for (int y = k - 1; y >= 0; y--) {
+                    int i = x + y*k;
+                    if (i < msg.length()) System.out.print(msg.charAt(i));
+                }
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
