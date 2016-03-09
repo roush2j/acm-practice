ad4674ba825cdc5e6ce6045066c4d0da08c9b257
JRoush <@>
2016-03-09 16:37:56 -0500

    Classification - optimizations & refactoring

--- src/ClassificationCmp.java
+++ src/ClassificationCmp.java
@@ -44,108 +44,130 @@ public class ClassificationCmp {
         randomTree(out, rand, vals, 0, N);
         out.println();
     }
-    
+
     public static void orderedInputLine(PrintStream out, int N, boolean order) {
         if (order) {
-            for (int i = 1; i < N; i++)out.append('(');
+            for (int i = 1; i < N; i++)
+                out.append('(');
             out.append('1');
-            for (int i = 1; i < N; i++){
+            for (int i = 1; i < N; i++) {
                 out.append(',');
                 out.print(i + 1);
                 out.append(')');
             }
         } else {
-            for (int i = N; i > 1; i--){
+            for (int i = N; i > 1; i--) {
                 out.append('(');
                 out.print(i);
                 out.append(',');
             }
             out.append('1');
-            for (int i = 1; i < N; i++)out.append(')');
+            for (int i = 1; i < N; i++)
+                out.append(')');
         }
         out.println();
     }
 
     /**
-     * Parse a parenthesis- and comma-delimited tree structure with exactly
-     * {@code N} leaves numbered {@code 1 .. N}.
-     * 
-     * @return An array where every 2 elements are a node, containing the index
-     *         of its parent node and the number of descendant nodes. The layout
-     *         is as follows:
-     * 
-     *         <pre>
-     * 0 <= i < 2*N     leaf with value i/2 + 1, contains index of parent and (1)
-     * 2*N == i         root node, contains -1 and (N)
-     * 2*N + 2 <= i     intermediate node, contains index of parent and (x)
-     * </pre>
+     * Parsed tree structure. The first N nodes are the leaves in order of
+     * value, the N+1 node is the root, and the remaining nodes follow.
      */
-    public static int[] parse(final int N, InputStream in) throws IOException {
-        final int logN = 32 - Integer.numberOfLeadingZeros(2 * N - 1);
-        StringBuilder buf = new StringBuilder();
-        int[] tree = new int[2 * N * logN];
-        int treec = 2 * N;
-        int root = -1;
+    public static class Tree {
+        /** Number of Leaves in the tree */
+        public final int   N;
 
-        for (int ch = in.read(); ch >= 0; ch = in.read()) {
-            switch (ch) {
-            case ' ':
-            case '\n':
-            case '\r':
-                break;  // skip whitespace
-            case '(':
-                if (treec == tree.length)
-                    tree = Arrays.copyOf(tree, treec * 2);
-                tree[treec++] = root;
-                tree[treec++] = 0;
-                root = treec - 2;
-                break;
-            case ')':
-            case ',':
-                if (buf.length() > 0) {
-                    int val = Integer.parseInt(buf.toString());
-                    tree[2 * val - 2] = root;
-                    tree[2 * val - 1] = 1;
-                    tree[root + 1]++;
-                    buf.setLength(0);
+        /** Index to the parent of the ith node */
+        public final int[] parent;
+
+        /** Number of leaves descending from the ith node */
+        public final int[] descCnt;
+
+        /** Index to the ith leaf node in depth-first visitation order */
+        public final int[] dforder;
+
+        /**
+         * Parse a parenthesis- and comma-delimited tree structure with exactly
+         * {@code N} leaves numbered {@code 1 .. N}.
+         */
+        public Tree(final int N, InputStream in) throws IOException {
+            this.N = N;
+            final int logN = 32 - Integer.numberOfLeadingZeros(2 * N - 1);
+            StringBuilder buf = new StringBuilder();
+            this.dforder = new int[N];
+            int leafc = 0;
+            int[] treepar = new int[N * logN];
+            int[] treecnt = new int[N * logN];
+            int treec = N;
+            int root = -1;
+
+            for (int ch = in.read(); ch >= 0; ch = in.read()) {
+                switch (ch) {
+                case ' ':
+                case '\n':
+                case '\r':
+                    break;  // skip whitespace
+                case '(':
+                    if (treec == treepar.length) {
+                        treepar = Arrays.copyOf(treepar, treec * 2);
+                        treecnt = Arrays.copyOf(treecnt, treec * 2);
+                    }
+                    treepar[treec++] = root;
+                    root = treec - 1;
+                    break;
+                case ')':
+                case ',':
+                    if (buf.length() > 0) {
+                        int val = Integer.parseInt(buf.toString());
+                        treepar[val - 1] = root;
+                        treecnt[val - 1] = 1;
+                        treecnt[root]++;
+                        dforder[leafc++] = val - 1;
+                        buf.setLength(0);
+                    }
+                    if (ch == ')') {
+                        int c = treecnt[root];
+                        root = treepar[root];
+                        if (root < 0) {
+                            this.parent = Arrays.copyOf(treepar, treec);
+                            this.descCnt = Arrays.copyOf(treecnt, treec);
+                            return;
+                        } else treecnt[root] += c;
+                    }
+                    break;
+                default:
+                    buf.append((char) ch);
+                    break;
                 }
-                if (ch == ')') {
-                    int c = tree[root + 1];
-                    root = tree[root];
-                    if (root < 0) return Arrays.copyOf(tree, treec);
-                    else tree[root + 1] += c;
-                }
-                break;
-            default:
-                buf.append((char) ch);
-                break;
             }
+
+            throw new RuntimeException("EOF");
         }
-        throw new RuntimeException("EOF");
-    }
 
-    public static void printTree(int N, int[] tree) {
-        int[] stack = new int[N];
-        for (int i = 0; i < N; i++) {
-            int stackc = 0;
-            for (int node = 2 * i; node >= 0; node = tree[node]) {
-                if (stackc == stack.length)
-                    stack = Arrays.copyOf(stack, stackc * 2);
-                stack[stackc++] = node;
+        /** Print the tree showing the descent of all nodes */
+        public void print(PrintStream out) {
+            int[] stack = new int[N];
+            for (int i = 0; i < N; i++) {
+                int stackc = 0;
+                for (int node = dforder[i]; node >= 0; node = parent[node]) {
+                    if (stackc == stack.length) {
+                        stack = Arrays.copyOf(stack, stackc * 2);
+                    }
+                    stack[stackc++] = node;
+                }
+                while (--stackc >= 0) {
+                    out.print(stack[stackc] + 1);
+                    out.print("(" + descCnt[stack[stackc]] + ")");
+                    out.print(' ');
+                }
+                out.println();
             }
-            while (--stackc >= 0) {
-                System.out.print(stack[stackc] / 2 + 1);
-                System.out.print("(" + tree[stack[stackc] + 1] + ")");
-                System.out.print(' ');
-            }
-            System.out.println();
         }
     }
 
     public static void main(String[] args) throws IOException {
 
-        if (false) {
-            System.setIn(new FileInputStream("Classification-in-50b"));
+        if (true) {
+            System.setIn(new FileInputStream("Classification-in-10000b"));
         }
 
         // parse number of leaves
@@ -163,7 +185,7 @@ public class ClassificationCmp {
             randomInputLine(out, new Random(), N);
             randomInputLine(out, new Random(), N);
             return;
-        } else if (true) {
+        } else if (false) {
             PrintStream out = new PrintStream("Classification-out");
             out.println(N);
             orderedInputLine(out, N, true);
@@ -173,65 +195,77 @@ public class ClassificationCmp {
         }
 
         // parse trees
-        int[] a = parse(N, System.in);
-        int[] b = parse(N, System.in);
-        //System.out.println(N);
-        //printTree(N, a);
-        //System.out.println();
-        //printTree(N, b);
-        //System.out.println();
+        Tree a = new Tree(N, System.in);
+        Tree b = new Tree(N, System.in);
+//        System.out.println(N);
+//        a.print(System.out);
+//        System.out.println();
+//        b.print(System.out);
+//        System.out.println();
 
         //
-        int[] nodemap = new int[2 * N];
-        for (int i = 0; i < 2 * N; i += 2) {
-            nodemap[i] = nodemap[i + 1] = i;
-        }
+        int[] anodes = Arrays.copyOf(a.dforder, N);
+        int[] bnodes = Arrays.copyOf(a.dforder, N);
         int matchingNodes = 0;
 
         // compare trees
-        for (int baseleaf = 0; true; baseleaf += 2) {
-            if (baseleaf >= 2 * N) baseleaf = 0;
+        for (int baseleaf = 0;;) {
+            if (baseleaf >= N) baseleaf = 0;
 
-            // check if all descendents have reached A-node
-            int anode = nodemap[baseleaf];
-            int acnt = 0;
-            for (int l = 0; l < N * 2; l += 2) {
-                if (nodemap[l] == anode) acnt++;
+            // find next node in A for which all descendents have been checked
+            // this relies on the assumption that, for *every* node in A,
+            // all descendant leaves are contiguous in our initial node ordering
+            final int anode = anodes[baseleaf];
+            final int acnt = a.descCnt[anode];
+            final int endleaf = baseleaf + acnt;
+            {
+                int l = baseleaf + 1;
+                for (; l < N && anodes[l] == anode; l++) {}
+                if (l != endleaf) {
+                    baseleaf = l;
+                    continue;
+                }
             }
-            if (acnt != a[anode + 1]) continue;
 
             // search upward until we find a common ancestor in B for
             // all leaves that descend from the target A-node
             int bcnt = acnt;
-            for (int l = baseleaf, ances = -1, match = 0; match < acnt; l += 2) {
-                if (l >= 2 * N) l = 0;
-                if (nodemap[l] != anode) continue;
+            for (int l = baseleaf, ances = -1, match = 0; match < acnt; l++) {
+                if (l >= endleaf) l = baseleaf;
 
                 // search upwards from leaf in B to find deepest node that 
                 // exceeds specified descendant count
-                int bnode = nodemap[l + 1];
-                for (; b[bnode + 1] < bcnt; bnode = b[bnode]) {}
+                int bnode = bnodes[l];
+                for (; b.descCnt[bnode] < bcnt; bnode = b.parent[bnode]) {}
                 if (bnode == ances) match++;
                 else {
-                    if (match > 0 && b[bnode + 1] == bcnt) bnode = b[bnode];
+                    if (match > 0 && b.descCnt[bnode] == bcnt)
+                        bnode = b.parent[bnode];
                     ances = bnode;
-                    bcnt = b[bnode + 1];
+                    bcnt = b.descCnt[bnode];
                     match = 1;
                 }
-                nodemap[l + 1] = bnode;
+                bnodes[l] = bnode;
             }
 
             // check if common ancestor is exact match for target A-node
             if (bcnt == acnt) {
                 matchingNodes++;
-            }
-
-            // advance A-node
-            int parent = a[anode];
-            for (int l = 0; l < N * 2; l += 2) {
-                if (nodemap[l] == anode) nodemap[l] = parent;
+//                System.out.print("{");
+//                for (int l = baseleaf; l < endleaf; l++) {
+//                    System.out.print(a.dforder[l] + 1);
+//                    System.out.append(',');
+//                }
+//                System.out.println("}");
             }
             if (acnt == N) break;
+
+            // advance A-node
+            int parent = a.parent[anode];
+            for (int l = baseleaf; l < endleaf; l++) {
+                anodes[l] = parent;
+            }
+            baseleaf = endleaf;
         }
 
         System.out.println(matchingNodes);
