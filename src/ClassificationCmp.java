8ab09d23c89470d7c83537b9c71fc62029387dcc
JRoush <@>
2016-03-09 14:31:25 -0500

    Classification - random tree generation
    test cases for debugging

--- src/ClassificationCmp.java
+++ src/ClassificationCmp.java
@@ -1,8 +1,49 @@
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.*;
 
 public class ClassificationCmp {
+
+    public static void randomTree(PrintStream out, Random rand, int[] vals,
+            int start, int len) {
+        if (len == 1) {
+            out.print(vals[start]);
+            return;
+        }
+        int children = 1 + Integer.numberOfLeadingZeros(rand.nextInt()) / 2;
+        if (children == 1) {
+            randomTree(out, rand, vals, start, len);
+            return;
+        } else if (children > len) children = len;
+        float childpop = len / (float) children;
+        int off = 0;
+        out.append('(');
+        for (int c = 1; c < children && off < len; c++) {
+            int l = Math.round(childpop * (rand.nextFloat() + rand.nextFloat())
+                    / 2.0F);
+            if (l == 0) l = 1;
+            randomTree(out, rand, vals, start + off, l);
+            out.append(',');
+            off += l;
+        }
+        if (off < len) {
+            randomTree(out, rand, vals, start + off, len - off);
+        }
+        out.append(')');
+    }
+
+    public static void randomInputLine(PrintStream out, Random rand, int N) {
+        int[] vals = new int[N];
+        for (int i = 0; i < vals.length; i++)
+            vals[i] = i;
+        for (int i = vals.length - 1; i >= 0; i--) {
+            int j = rand.nextInt(i + 1);
+            int tmp = vals[i];
+            vals[i] = vals[j];
+            vals[j] = tmp;
+        }
+        randomTree(out, rand, vals, 0, N);
+        out.println();
+    }
 
     /**
      * Parse a parenthesis- and comma-delimited tree structure with exactly
@@ -81,24 +122,81 @@ public class ClassificationCmp {
     }
 
     public static void main(String[] args) throws IOException {
+
+        // parse number of leaves
         StringBuilder sb = new StringBuilder();
         for (int ch = System.in.read(); ch >= 0; ch = System.in.read()) {
             if (ch >= '0' && ch <= '9') sb.append((char) ch);
             else break;
         }
         final int N = Integer.parseInt(sb.toString());
+
+        // randomized inputs
+        if (true) {
+            PrintStream out = new PrintStream("Classification-in-" + N);
+            out.println(N);
+            randomInputLine(out, new Random(), N);
+            randomInputLine(out, new Random(), N);
+            return;
+        }
+
+        // parse trees
         int[] a = parse(N, System.in);
         int[] b = parse(N, System.in);
 
-        int[] nodet = new int[2 * N];
+        //
+        int[] nodemap = new int[2 * N];
         for (int i = 0; i < 2 * N; i += 2) {
-            nodet[i] = nodet[i + 1] = i;
+            nodemap[i] = nodemap[i + 1] = i;
         }
-        int matchcnt = N;
-        
-        
-        printTree(N, a);
-        System.out.println();
-        printTree(N, b);
+        int matchingNodes = 0;
+
+        // compare trees
+        for (int baseleaf = 0; true; baseleaf += 2) {
+            if (baseleaf >= 2 * N) baseleaf = 0;
+
+            // check if all descendents have reached A-node
+            int anode = nodemap[baseleaf];
+            int acnt = 0;
+            for (int l = 0; l < N * 2; l += 2) {
+                if (nodemap[l] == anode) acnt++;
+            }
+            if (acnt != a[anode + 1]) continue;
+
+            // search upward until we find a common ancestor in B for
+            // all leaves that descend from the target A-node
+            int bcnt = acnt;
+            for (int l = baseleaf, ances = -1, match = 0; match < acnt; l += 2) {
+                if (l >= 2 * N) l = 0;
+                if (nodemap[l] != anode) continue;
+
+                // search upwards from leaf in B to find deepest node that 
+                // exceeds specified descendant count
+                int bnode = nodemap[l + 1];
+                for (; b[bnode + 1] < bcnt; bnode = b[bnode]) {}
+                if (bnode == ances) match++;
+                else {
+                    if (match > 0 && b[bnode + 1] == bcnt) bnode = b[bnode];
+                    ances = bnode;
+                    bcnt = b[bnode + 1];
+                    match = 1;
+                }
+                nodemap[l + 1] = bnode;
+            }
+
+            // check if common ancestor is exact match for target A-node
+            if (bcnt == acnt) {
+                matchingNodes++;
+            }
+
+            // advance A-node
+            int parent = a[anode];
+            for (int l = 0; l < N * 2; l += 2) {
+                if (nodemap[l] == anode) nodemap[l] = parent;
+            }
+            if (acnt == N) break;
+        }
+
+        System.out.println(matchingNodes);
     }
 }
