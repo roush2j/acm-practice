297af0b291d781b27f427bad268d0863c87815e9
JRoush <@>
2016-09-12 10:04:59 -0400

    Tweaked wettiles to make it closer to true Dijkstra

--- src/WetTiles.java
+++ src/WetTiles.java
@@ -7,11 +7,10 @@ import java.util.*;
  * less than some specified threshold.
  * 
  * This is a textbook use case for best-first search such as Dijkstra's
- * Algorithm. We start with a set (#1) of leak tiles. All of the non-wall
- * neighbors of the leak tiles are added to another set (#2). Then all of the
- * non-wall, not-yet-in-any-set neighbors of the #2 tiles are added to yet
- * another set (#3). Then the neighbors of the #3 tiles are added to #4, and so
- * on until we have a set for each # up to the given threshold.
+ * Algorithm. We start with a FIFO queue of leak tiles a distance 1. All of the
+ * non-wall neighbors of the leak tiles are added to the queue with distance 2.
+ * Then all of the non-wall, not-yet-queued neighbors of those tiles are added,
+ * and so on until we have reached the distance threshold.
  * 
  * https://open.kattis.com/contests/mk2hhn/problems/wettiles
  * 
@@ -41,12 +40,12 @@ public class WetTiles {
             final int TILE_WALL = 2;
 
             // read leak positions and mark them
-            List<Integer> curset = new ArrayList<>();
+            Queue<Integer> frontier = new ArrayDeque<Integer>();
             for (int i = 0; i < leakCnt; i++) {
                 int x = s.nextInt() - 1, y = s.nextInt() - 1;
                 int tile = y * width + x;
                 tiles[tile] = TILE_WET; // all leaks become wet at minute 1
-                curset.add(tile); // add leaks to first tile set
+                frontier.add(tile); // add leaks to first tile set
             }
 
             // read wall positions and mark them
@@ -64,40 +63,36 @@ public class WetTiles {
 
             // count all tiles at most maxTime units away from the nearest leak
             // using Dijkstra's algorithm
-            int wetTiles = leakCnt;
-            List<Integer> nextset = new ArrayList<>();
-            for (int t = 2; t <= maxTime && !curset.isEmpty(); t++) {
+            int wetTiles = 0;
+            for (int t = 1; t <= maxTime && !frontier.isEmpty(); t++) {
 
-                // iterate over tiles in current set (newly marked)
-                for (int tile : curset) {
-                    // for each tile, add unmarked neigbors to next set
+                // iterate over tiles in current set (newly visited)
+                for (int i = frontier.size(); i > 0; i--) {
+                    // pop tile off of queue
+                    wetTiles++;
+                    int tile = frontier.remove();
                     int x = tile % width, y = tile / width;
+                    // for each tile, add dry (unvisited) neighbors to queue
                     int left = y * width + x - 1, right = y * width + x + 1;
                     int up = (y - 1) * width + x, down = (y + 1) * width + x;
                     if (x > 0 && tiles[left] == TILE_DRY) {
                         tiles[left] = TILE_WET;
-                        nextset.add(left);
+                        frontier.add(left);
                     }
                     if (x + 1 < width && tiles[right] == TILE_DRY) {
                         tiles[right] = TILE_WET;
-                        nextset.add(right);
+                        frontier.add(right);
                     }
                     if (y > 0 && tiles[up] == TILE_DRY) {
                         tiles[up] = TILE_WET;
-                        nextset.add(up);
+                        frontier.add(up);
                     }
                     if (y + 1 < height && tiles[down] == TILE_DRY) {
                         tiles[down] = TILE_WET;
-                        nextset.add(down);
+                        frontier.add(down);
                     }
                 }
-                wetTiles += nextset.size();
 
-                // discard contents of current set and make the next set the current set
-                List<Integer> _tmp = curset;
-                curset = nextset;
-                nextset = _tmp;
-                nextset.clear();
             }
 
             // print results
