6284c9c8c711c5f0f35b4041d6da2be62a38f37a
JRoush <@>
2016-10-19 21:25:01 -0400

    Poly with full explanation

--- src/PlottingPolynomials.java
+++ src/PlottingPolynomials.java
@@ -6,24 +6,49 @@ import java.util.*;
  * Taylor's series but uses finite differences instead of derivatives. Like
  * Taylor's series, Newton's series is often used for building approximations of
  * functions, and it is particularly well-suited to the discrete, iterative
- * approximations used in many numerical simulations. 
+ * approximations used in many numerical simulations.
  * <p>
- * Newton's series builds up a polynomial representation of a function by
- * sampling it at discrete points {@code x0, x0+dx, x0+2*dx, ...} :
+ * In this problem, we are given a polynomial of degree N with coefficients
+ * {@code a0, a1, a2, ... aN} such that
+ * 
+ * <pre>
+ *    p(x) = SUM from i = 0 to N of ( ai * x^i )
+ * </pre>
+ * 
+ * The problem posits an efficient algorithm for sampling this polynomial using
+ * some mystery coefficients {@code C0, C1, C2, ... CN}:
+ * 
+ * <pre>
+ *    p(0) = C0,         t10 = C1,         t20 = C2,         ...   tN0 = CN
+ *    p(1) = p(0)+t10,   t11 = t10+t20,    t21 = t20+t30,    ...   tN1 = CN
+ *    p(2) = p(1)+t11,   t12 = t11+t21,    t22 = t21+t31,    ...   tN2 = CN
+ *        ...               ...               ...                     ...
+ *  p(x+1) = p(x)+t1x, t1x+1 = t1x+t2x,  t2x+1 = t2x+t3x,    ... tNx+1 = CN
+ * </pre>
+ * 
+ * If we work backwards to fill in all the variables {@code t} for the
+ * appropriate combinations of constants C, we find:
+ * 
+ * <pre>
+ *    p(0) = C0,         t10 = C1,         t20 = C2,         ...   tN0 = CN
+ *    p(1) = C0+C1,      t11 = C1+C2,      t21 = C2+C3,      ...   tN1 = CN
+ *    p(2) = C0+2*C1+C2, t12 = C1+2*C2+C3, t22 = C2+2*C3+C4, ...   tN2 = CN
+ *        ...
+ *    p(x) = SUM from L = 0 to x of ( (x choose L) * C_L )
+ * </pre>
+ * 
+ * This is very similar to the form of Newton's series for general functions:
  * 
  * <pre>
  *      F(x0 + m*dx) = 
- *         F(x0)
- *         + m * ( F(x0 + dx) - F(x0) )
- *         + (1/2)*m*(m-1) * ( F(x0 + 2*dx) - 2*F(x0 + dx) + F(x0) )
- *         + (1/6)*m*(m-1)*(m-2) * ( F(x0 + 3*dx) - 3*F(x0 + 2*dx) + 3*F(x0 + dx) - F(x0) )
- *         + ...
- *               
- *        = SUM from L = 0 to CUTOFF of 
- *          (m choose L) * (Lth forward difference of F(x0) in steps of dx)
+ *          SUM from L = 0 to N of 
+ *              (m choose L) * (Lth forward difference of F(x0) in steps of dx)
+ *      
+ *      take x0 = 0, dx = 1, m = x, so that F(x0 + m*dx) == p(x)
  * </pre>
- * 
- * where:
+ *
+ * Therefore the coefficients {@code C_L} that we are interested in are exactly
+ * the "Lth forward differences":
  * 
  * <pre>
  *      Lth forward difference of F(x0) in steps of dx =
@@ -31,13 +56,11 @@ import java.util.*;
  *              (-1)^(L-y) * (L choose y) * F(x0 + y*dx)
  * </pre>
  * 
- * If we take CUTOFF to be infinity, Newton's series is an infinite series which
- * perfectly represents our function. If we take CUTOFF to be N, then Newton's
- * series is an Nth order polynomial which passes through the points
- * {@code x0, x0+dx, x0+2*dx, ..., x0+N*dx} . With CUTOFF=1, for example, we
- * would obtain a straight line passing through F(x0) and F(xo + dx).
- * <p>
+ * and therefore:
  * 
+ * <pre>
+ *      C_L = SUM from y = 0 to L of  ( (-1)^(L-y) * (L choose y) * p(y) )
+ * </pre>
  * 
  * https://open.kattis.com/contests/rt95cv/problems/plot
  *
@@ -58,27 +81,31 @@ public class PlottingPolynomials {
 
         // precompute the values of the polynomial p(0) ... p(N)
         int[] p = new int[N + 1];
-        for (int x = 0; x <= N; x++) {
-            int xi = 1;
+        for (int y = 0; y <= N; y++) {
+            int yi = 1;
             for (int i = 0; i < a.length; i++) {
-                p[x] += a[i] * xi;
-                xi *= x;
+                p[y] += a[i] * yi;
+                yi *= y;
             }
         }
 
-        // compute the coefficients C_0 ... C_N (the nth forward difference)
-        int[] t = new int[N + 1];
-        t[0] = 1;
-        System.out.print(p[0]);
-        for (int l = 1; l <= N; l++) {
+        // compute the coefficients C_0 ... C_N (the Lth forward difference)
+        int[] choose = new int[N + 1];
+        choose[0] = 1;
+        System.out.print(p[0]); // first constant C0
+        for (int L = 1; L <= N; L++) {
             int C = 0;
-            for (int x = l; x > 0; x--) {
-                t[x] = t[x - 1] - t[x];
-                C += t[x] * p[x];
+            for (int y = L; y > 0; y--) {
+                // we calculate the values of (-1)^(L-y) 
+                // from the previous values (-1)^(L-y-1) * (L-1 choose y)
+                // we do this in-place, overwriting the old values,
+                // which is why we iterate backwards
+                choose[y] = choose[y - 1] - choose[y];
+                C += choose[y] * p[y];
             }
-            t[0] = -t[0];
-            C += t[0] * p[0];
-            System.out.print(" " + C);
+            choose[0] = -choose[0]; // calculate new value of (-1)^L for y==0
+            C += choose[0] * p[0];
+            System.out.print(" " + C); // print C_L
         }
     }
 }
